33bca8ae2e9c284f65534b3076a89e05e1930875
sabarim6369 <sabarim6369@gmail.com>
2025-05-17 21:39:07 +0530

    added different emoji

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express();
 app.get("/",(req,res)=>{
-    res.send("Hello world😍😍😍");
+    res.send("Bye world");
 })
 app.listen(8000,'0.0.0.0',()=>{
 console.log("Server running in port 8000")
